698b88515623c7fa3f25330537c41561554d13a4
Cody Williams <cody122008@icloud.com>
2026-05-29 12:38:04 -0700

    Minor fixes for consistent indentation and structure
    Called the function at the end.

--- cody.py
+++ cody.py
@@ -1,35 +1,36 @@
 # "Educational Experience" based on the book Evicted by Matthew Desmond.
 
 def dsj_topic():
-    return "Evicted: Poverty and Profit in the American City"
 
-print("Based on the book, Evicted: Poverty and Profit in the American City\n")
+    print("Based on the book, Evicted: Poverty and Profit in the American City\n")
 
-# Introduction
-print("--- Welcome! ---")
-print("Evicted follows families struggling to afford housing.")
-print("The book explores how poverty and eviction affect people's lives.\n")
-print("''it is hard to argue that housing is not a fundamental human need. Decent, affordable housing should be a")
-print("basic right for everybody in this country. The reason is simple: without stable shelter, everything else falls apart.''")
-print("― Matthew Desmond, Evicted.\n")
+    # Introduction
+    print("--- Welcome! ---")
+    print("Evicted follows families struggling to afford housing.")
+    print("The book explores how poverty and eviction affect people's lives.\n")
+    print("''it is hard to argue that housing is not a fundamental human need. Decent, affordable housing should be a")
+    print("basic right for everybody in this country. The reason is simple: without stable shelter, everything else falls apart.''")
+    print("― Matthew Desmond, Evicted.\n")
 
-answer = input("What is eviction?\nA. Leaving school\nB. Losing your home\nC. Buying food\nAnswer: ")
+    answer = input("What is eviction?\nA. Leaving school\nB. Losing your home\nC. Buying food\nAnswer: ")
 
-if answer.upper() == "B":
-    print("Correct! Thank you for learning about Evicted!\n")
-else:
-    print("The correct answer is B.\n")
+    if answer.upper() == "B":
+        print("Correct! Thank you for learning about Evicted!\n")
+    else:
+        print("The correct answer is B.\n")
+        
+    answer = input("How can eviction affect children?\nA. More stress\nB. Better sleep\nC. More money\nAnswer: ")
+
+    if answer.upper() == "A":
+        print("Correct!")
+    else:
+        print("The correct answer is A. More stress.")
+        
+    choice = input("\nWhy do you believe housing is important?: ")
+
+    print("\nYour answer:")
+    print(choice)
+
+    print("\nThanks for learning about Evicted!")
     
-answer = input("How can eviction affect children?\nA. More stress\nB. Better sleep\nC. More money\nAnswer: ")
-
-if answer.upper() == "A":
-    print("Correct!")
-else:
-    print("The correct answer is A. More stress.")
-    
-choice = input("\nWhy do you believe housing is important?: ")
-
-print("\nYour answer:")
-print(choice)
-
-print("\nThanks for learning about Evicted!")
+dsj_topic()
