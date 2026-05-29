7f537128fa4b6f690256c102d20727ddd5d64332
charliehorne <75321790+charliehorne@users.noreply.github.com>
2026-05-29 12:09:04 -0700

    Update charlie.py

--- charlie.py
+++ charlie.py
@@ -1,2 +1,8 @@
 def dsj_topic():
-  print("racism in hiring practices")
+print("Hiring Practices Experience Survey")
+response = input("Have you experienced discrimination when looking for a job? (yes/no): ")
+if response.lower() == "yes":
+    print("Thank you for sharing.")
+    details = input("Then I have a book for you if you're interested. It's called Stories employers Tell by Phillip Moss and Chris Tilly: ")
+else: 
+        print("Then you are very fortunate.")
