347953dee8ba9f2104c5869439fd24fa9baa85d6
Cody Williams <cody122008@icloud.com>
2026-06-03 11:54:40 -0700

    Added eviction quiz questions, and fixed minor bugs.

--- cody.py
+++ cody.py
@@ -12,25 +12,37 @@ def dsj_topic():
     print("basic right for everybody in this country. The reason is simple: without stable shelter, everything else falls apart.''")
     print("― Matthew Desmond, Evicted.\n")
 
-    answer = input("What is eviction?\nA. Leaving school\nB. Losing your home\nC. Buying food\nAnswer: ")
+    answer = input("What is eviction?\nA. Leaving school\nB. Losing your home\nC. Buying food\n\nAnswer: ")
 
     if answer.upper() == "B":
-        print("Correct! Thank you for learning about Evicted!\n")
+        print("Correct!\n")
     else:
         print("The correct answer is B.\n")
-        
-    answer = input("How can eviction affect children?\nA. More stress\nB. Better sleep\nC. More money\nAnswer: ")
+
+    answer = input("How can eviction affect children?\nA. More stress\nB. Better sleep\nC. More money\n\nAnswer: ")
 
     if answer.upper() == "A":
-        print("Correct!")
+        print("Correct!\n")
     else:
         print("The correct answer is A. More stress.")
         
+    choice = input(
+    "\nYou only have enough money for one:\n"
+    "A. Rent\n"
+    "B. Groceries\n"
+    "C. Gas\n"
+    "D. Medicine\n"
+    "Which would you choose?: "
+    )
+
+    print("Families facing poverty often have to make difficult choices like these.")
+    
+
     choice = input("\nWhy do you believe housing is important?: ")
 
     print("\nYour answer:")
     print(choice)
 
     print("\nThanks for learning about Evicted!")
-    
+
 dsj_topic()
