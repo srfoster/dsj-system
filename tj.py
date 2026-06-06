ef61b0476cb01a8be39826dea505a41bac7b9efe
TJ Wegeleben <thomas.wegeleben.ii@gmail.com>
2026-06-06 12:12:33 -0700

    Initial framework

--- tj.py
+++ tj.py
@@ -1,2 +1,13 @@
 def dsj_topic():
-    print("gender identity")
+    main_menu()
+
+def main_menu():
+    menu_header("Main Menu")
+    print("Trans Technologies by Oliver L. Haimson")
+
+def menu_header(title):
+    print("=" * len(title))
+    print(title)
+    print("=" * len(title))
+
+#dsj_topic()
