4ab1815dac18ffaaf225e87e31a2833135e7e76a
Ian Palmer <ianpalmer2004@gmail.com>
2026-06-11 14:46:57 -0700

    Update ashlan_ian_isaac_two.py
    added Ian's part

--- ashlan_ian_isaac_two.py
+++ ashlan_ian_isaac_two.py
@@ -1,2 +1,21 @@
 def dsj_topic():
   print ("weapons of math destruction chapter 3")
+  
+  #ian
+  def ToF(question, answer):
+    print(question)
+    guess = input('True or False: ')
+    
+    if guess == answer:
+        print('Correct\n')
+        
+    else:
+        print("Incorrect\n")
+  
+  question = "Baylor University paid for admitted students to retake the SAT, hoping another try would boost their scores"
+  answer = "true"
+  ToF(question,answer)
+  
+  question = "Our society does not care for college education and does not value it."
+  answer = "false"
+  ToF(question,answer)
