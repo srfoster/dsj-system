4a2bf81775803888540f9cda55c875e92d5cf3c8
1nsidious <killacake@ymail.com>
2026-06-13 22:29:04 -0700

    made the char function usable

--- cody_raphael_tj_two.py
+++ cody_raphael_tj_two.py
@@ -2,27 +2,21 @@
 
 import random  
 def char():
-    race = ["Elf", "Orc", "Dwarf", "Mark Zuckerberg (Lizardmen)", "Goblin", "Human", "Slime", "Hobgoblin", "Troll", "Ogre", "High Elf", "Dark Elf", "Halfling"]
-    residence = ["whiterun", "Kvatch", "Jacobstown", "Jacinto", "Lordran", "Yharnam", "Raccoon City", "Wraeclast"]
-    personality = ["Low Value", "Mid Value", "High Value"]
-    gold = range(-200000,200000)
-    traits = ["Dim Witted", "Quick Learner", "Undead", ]
-    ailments = ["Cursed", "Vampirism", "Lycanthropy",]
-    classtype = ["Necromancer", "Thief", "Cleric", "Warrior", "Tank", "Druid", "Ranger", "Mage", "Assassin", "Paladin", "Healer"]
 
     character = {
-        Race: random.choice(races),
-        Residence: random.choice(races),
-        Personality: random.choice(races),
-        Gold: random.randint(-200000,200000),
-        Traits: random.sample(races, random.randint(1,3)),
-        Ailments: random.choice(races),
-        Class: random.choice(races),
+        "Race": random.choice(["Elf", "Orc", "Dwarf", "Mark Zuckerberg (Lizardmen)", "Goblin", "Human", "Slime", "Hobgoblin", "Troll", "Ogre", "High Elf", "Dark Elf", "Halfling"]),
+        "Residence": random.choice(["whiterun", "Kvatch", "Jacobstown", "Jacinto", "Lordran", "Yharnam", "Raccoon City", "Wraeclast"]),
+        "Personality": random.choices(["Low Value", "Mid Value", "High Value"], [5,4,1])[0], #these numbers are weights
+        "Gold": random.randint(-200000,200000),
+        "Traits": random.sample(["Dim Witted", "Quick Learner", "Undead", "Cannibal", "Lucky", "Anger Issues", "Pyromaniac", "Deaf", "Blind", "Stupid", ], random.randint(1,3))[0],
+        "Ailments": random.choices(["Cursed", "Vampirism", "Lycanthropy", "None",], [1,1,1,3])[0],
+        "Class": random.choice(["Necromancer", "Thief", "Cleric", "Warrior", "Tank", "Druid", "Ranger", "Mage", "Assassin", "Paladin", "Healer"]),
     }
-    print(character())
+    #print(character)
+    return character #this is a dictionary with all the random character attributes
 '''
 def rule():
-    rules = no faith, no vam/lycan , no goblin/hobgoblib, no short races, debt limit(+-), no undead/vampire, no rogue classes, no tanky classes, no tall races,
+    rules = no faith, no vam/lycan , no goblin/hobgoblib, no short races, debt limit(+-), no undead/vampire, no rogue classes, no tanky classes, no tall races, high values always accepted, no ailments, 
     new_rule = random.choice(rules)
     return rule or maybe all active rules(not sure yet)
 ''' 
